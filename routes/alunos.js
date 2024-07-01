f469ee8bd17f99478bc58c74da0aaa6ece743c62
Prof. Hugo <hugo.tannus@go.senac.br>
2024-07-01 18:34:03 -0300

    Adiciona exemplo de utilização de expressões para ler dados no handlebars.

--- routes/alunos.js
+++ routes/alunos.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var alunos = require('../tests/mocks/alunos.json')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('read', { title: 'Alunos' });
+    res.render('read', { title: 'Alunos', alunos });
 });
 
 router.get('/new', function (req, res, next) {
-    res.render('form', { title: 'Novo Aluno' });
+    res.render('form', { title: 'Novo Aluno',  buttonText: 'Adicionar' });
 });
 
-router.get('/edit/:id', function (req, res, next) {
-    res.render('form', { title: 'Editar Aluno' });
+router.get('/edit/:matricula', function (req, res, next) {
+    const { matricula } = req.params;
+    
+    const keys = Object.keys(alunos.heads);
+    const aluno = alunos.content.find(aluno => aluno.matricula == matricula);
+
+    res.render('form', { keys, aluno, labels: alunos.heads, title: 'Editar Aluno', buttonText: 'Salvar' });
 });
 
 module.exports = router;
