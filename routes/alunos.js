5b167449e61bee1198b0d700fd57e9f2406e2b04
Prof. Hugo <hugo.tannus@go.senac.br>
2024-07-03 18:07:50 -0300

    Ajusta rota e _views_ de detalhes de alunos.

--- routes/alunos.js
+++ routes/alunos.js
@@ -5,20 +5,19 @@ let alunos = require('../tests/mocks/alunos.json');
 /* GET users listing. */
 router.get('/', function (_req, res, next) {
     const data = { title: 'Alunos', alunos };
-    // res.render('read', data);
     res.render('list', data);
 });
 
 router.get('/new', function (_req, res, next) {
-    res.render('form', { title: 'Novo Aluno',  buttonText: 'Adicionar' });
+    res.render('form', { title: 'Novo Aluno',  buttonText: 'Adicionar Aluno' });
 });
 
 router.get('/:matricula', function (req, res, next) {
     const { matricula } = req.params;
-    
+ 
     const aluno = alunos.content[matricula];
 
-    res.render('read_one', { title: 'Detalhes do Aluno', buttonText: 'Salvar', labels: alunos.heads, ...aluno });
+    res.render('card', { aluno, title: 'Detalhes do Aluno'});
 });
 
 router.get('/edit/:matricula', function (req, res, next) {
@@ -26,7 +25,7 @@ router.get('/edit/:matricula', function (req, res, next) {
     
     const aluno = alunos.content[matricula];
 
-    res.render('form', { title: 'Editar Aluno', buttonText: 'Salvar', labels: alunos.heads, aluno });
+    res.render('form', { aluno, title: 'Editar Aluno', buttonText: 'Salvar Alterações' });
 });
 
 module.exports = router;
