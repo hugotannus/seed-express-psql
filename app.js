25e71d916d05fde81eb0604a40c606d96b19e72d
Prof. Hugo <hugo.tannus@go.senac.br>
2024-07-04 18:42:51 -0300

    Instala pacote `body-parser`

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var alunosRouter = require('./routes/alunos');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(bodyParser.urlencoded());
 
 // view engine setup
 // https://github.com/ericf/express-handlebars?tab=readme-ov-file#extnamehandlebars
